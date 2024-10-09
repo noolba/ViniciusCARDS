c38363a0232ed13bb3bcd7897702e3d73b650f1c
noolba <163531500+noolba@users.noreply.github.com>
2024-10-09 09:29:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem ganhou a copa do mundo de 2002?',
+    'O Brasil '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sorvete em Inglês?',
+    'Sorvete em ingles é ice cream'
+)
